5599babc15b27ea40cc3a5a82a5a5eb982e7c0f5
Nick Hale <nhale25@gmail.com>
2014-02-04 19:34:54 +0000

    Made final column of objectlist auto-resize itself to fit.

--- controls/objectList.py
+++ controls/objectList.py
@@ -1,52 +1,52 @@
 
 import wx
 import wx.lib.agw.ultimatelistctrl as ULC
+import wx.lib.mixins.listctrl as listmix
+
 from memUsageGauge import MemUsageGauge
 from guiHelpers import Event
 
-class ObjectList(wx.Panel):
+class ObjectList(ULC.UltimateListCtrl, listmix.ListCtrlAutoWidthMixin):
 	def __init__(self, *args, **kwargs):
-		wx.Panel.__init__(self, *args, **kwargs)
+		kwargs["agwStyle"] = wx.LC_REPORT | wx.LC_VRULES | wx.LC_HRULES | wx.LC_SINGLE_SEL | ULC.ULC_NO_HIGHLIGHT | ULC.ULC_HAS_VARIABLE_ROW_HEIGHT
+		ULC.UltimateListCtrl.__init__(self, *args, **kwargs)
+		listmix.ListCtrlAutoWidthMixin.__init__(self)
+		
 		self._numberFormatter = lambda x: "%d"% x
 		
-		self._listCtrl = ULC.UltimateListCtrl(self, wx.ID_ANY,
-							agwStyle=wx.LC_REPORT | wx.LC_VRULES | wx.LC_HRULES | wx.LC_SINGLE_SEL | ULC.ULC_NO_HIGHLIGHT | ULC.ULC_HAS_VARIABLE_ROW_HEIGHT)
-		self._listCtrl.InsertColumn(0, "Symbol name", width=200)
-		self._listCtrl.InsertColumn(1, "Size", width=50)
-		self._listCtrl.InsertColumn(2, "", width=200)
-		
-		sizer = wx.BoxSizer()
-		sizer.Add(self._listCtrl, 1, wx.EXPAND)
-		self.SetSizer(sizer)
+		self.InsertColumn(0, "Symbol name", width=150)
+		self.InsertColumn(1, "Size", width=50)
+		self.InsertColumn(2, "")
+		self.setResizeColumn(3)
 	
 	def setNumberFormatter(self, formatter):
 		self._numberFormatter = formatter
 		
-		for pos in range(self._listCtrl.GetItemCount()):
-			obj = self._listCtrl.GetItemData(pos)
+		for pos in range(self.GetItemCount()):
+			obj = self.GetItemData(pos)
 			sizeStr = self._numberFormatter(obj.size)
-			self._listCtrl.SetStringItem(pos, 1, sizeStr)
+			self.SetStringItem(pos, 1, sizeStr)
 	
 	def _addRow(self, symbol, largestSymbolSize, symbolType):
-		pos = self._listCtrl.InsertStringItem(0, symbol.name)
-		self._listCtrl.SetItemData(pos, symbol)
+		pos = self.InsertStringItem(0, symbol.name)
+		self.SetItemData(pos, symbol)
 		
 		sizeStr = self._numberFormatter(symbol.size)
-		self._listCtrl.SetStringItem(pos, 1, sizeStr)
+		self.SetStringItem(pos, 1, sizeStr)
 		
-		gauge = MemUsageGauge(self._listCtrl, wx.ID_ANY, style=wx.GA_HORIZONTAL, size=(-1, 20))
+		gauge = MemUsageGauge(self, wx.ID_ANY, style=wx.GA_HORIZONTAL, size=(-1, 20))
 		gauge.SetCapacity(largestSymbolSize)
 		gauge.SetSizes(**{symbolType:symbol.size})
-		self._listCtrl.SetItemWindow(pos, col=2, wnd=gauge, expand=True)
+		self.SetItemWindow(pos, col=2, wnd=gauge, expand=True)
 	
 	def updateInfo(self, codeSymbols, initDataSymbols, roDataSymbols):
 		#workaround for bug in certain versions of UltimateListCtrl when calling DeleteAllItems()
-		for item in self._listCtrl._mainWin._itemWithWindow[:]:
+		for item in self._mainWin._itemWithWindow[:]:
 			if item.GetWindow():
-				self._listCtrl._mainWin.DeleteItemWindow(item)
+				self._mainWin.DeleteItemWindow(item)
 		#end of workaround
 		
-		self._listCtrl.DeleteAllItems()
+		self.DeleteAllItems()
 		
 		if codeSymbols is not None and initDataSymbols is not None and roDataSymbols is not None:
 			largest = max([obj.size for obj in codeSymbols + initDataSymbols + roDataSymbols])
@@ -57,4 +57,4 @@ class ObjectList(wx.Panel):
 			for sym in roDataSymbols:
 				self._addRow(sym, largest, "readOnly")
 			
-			self._listCtrl.SortItems(lambda x, y: y.size - x.size)
+			self.SortItems(lambda x, y: y.size - x.size)
