ae527b0ea1620ad60ee4e66293a9670c24356914
Nick Hale <nhale25@gmail.com>
2014-02-04 22:38:18 +0000

    Added a column for Symbol Type on the symbol list.
    This avoids having to rely only on the colors of the bar-charts.

--- controls/symbolList.py
+++ controls/symbolList.py
@@ -7,19 +7,26 @@ from memUsageGauge import MemUsageGauge
 from guiHelpers import Event
 
 class SymbolList(ULC.UltimateListCtrl, listmix.ListCtrlAutoWidthMixin, listmix.ColumnSorterMixin):
+	symbolTypeNames = {
+		"code":"Code",
+		"initialized":"Initialised data",
+		"readOnly":"Read-only data",
+		}
+	
 	def __init__(self, *args, **kwargs):
 		kwargs["agwStyle"] = wx.LC_REPORT | wx.LC_VRULES | wx.LC_HRULES | wx.LC_SINGLE_SEL | ULC.ULC_NO_HIGHLIGHT | ULC.ULC_HAS_VARIABLE_ROW_HEIGHT
 		ULC.UltimateListCtrl.__init__(self, *args, **kwargs)
 		listmix.ListCtrlAutoWidthMixin.__init__(self)
-		listmix.ColumnSorterMixin.__init__(self, 3)
+		listmix.ColumnSorterMixin.__init__(self, 4)
 		self.itemDataMap = {}
 		
 		self._numberFormatter = lambda x: "%d"% x
 		
 		self.InsertColumn(0, "Symbol name", width=150)
-		self.InsertColumn(1, "Size", width=50)
-		self.InsertColumn(2, "")
-		self.setResizeColumn(3)
+		self.InsertColumn(1, "Type", width=100)
+		self.InsertColumn(2, "Size", width=50)
+		self.InsertColumn(3, "")
+		self.setResizeColumn(4)
 	
 	def setNumberFormatter(self, formatter):
 		self._numberFormatter = formatter
@@ -32,15 +39,18 @@ class SymbolList(ULC.UltimateListCtrl, listmix.ListCtrlAutoWidthMixin, listmix.C
 	def _addRow(self, symbol, largestSymbolSize, symbolType):
 		pos = self.InsertStringItem(0, symbol.name)
 		self.SetItemData(pos, symbol)
-		self.itemDataMap[symbol] = (symbol.name, symbol.size, symbol.size)
+		self.itemDataMap[symbol] = (symbol.name, symbolType, symbol.size, symbol.size)
+		
+		typeStr = self.symbolTypeNames[symbolType]
+		self.SetStringItem(pos, 1, typeStr)
 		
 		sizeStr = self._numberFormatter(symbol.size)
-		self.SetStringItem(pos, 1, sizeStr)
+		self.SetStringItem(pos, 2, sizeStr)
 		
 		gauge = MemUsageGauge(self, wx.ID_ANY, style=wx.GA_HORIZONTAL, size=(-1, 20))
 		gauge.SetCapacity(largestSymbolSize)
 		gauge.SetSizes(**{symbolType:symbol.size})
-		self.SetItemWindow(pos, col=2, wnd=gauge, expand=True)
+		self.SetItemWindow(pos, col=3, wnd=gauge, expand=True)
 	
 	def updateInfo(self, codeSymbols, initDataSymbols, roDataSymbols):
 		#workaround for bug in certain versions of UltimateListCtrl when calling DeleteAllItems()
@@ -61,7 +71,7 @@ class SymbolList(ULC.UltimateListCtrl, listmix.ListCtrlAutoWidthMixin, listmix.C
 			for sym in roDataSymbols:
 				self._addRow(sym, largest, "readOnly")
 			
-			self.SortListItems(1, False)
+			self.SortListItems(2, False)
 			
 	#Required by listmix.ColumnSorterMixin
 	def GetListCtrl(self):
@@ -70,3 +80,18 @@ class SymbolList(ULC.UltimateListCtrl, listmix.ListCtrlAutoWidthMixin, listmix.C
 	#Required by listmix.ColumnSorterMixin
 	def OnSortOrderChanged(self):
 		self.Refresh()
+	
+	def GetSecondarySortValues(self, col, key1, key2):
+		#If sorting by name or symbol type, secondary sort should always be by size, and descending
+		if col == 0 or col == 1:
+			value1 = self.itemDataMap[key1][2]
+			value2 = self.itemDataMap[key2][2]
+			
+			_, dirn = self.GetSortState()
+			if dirn:
+				return (value2, value1)
+			else:
+				return (value1, value2)
+		else:
+			return listmix.ColumnSorterMixin.GetSecondarySortValues(self, col, key1, key2)
+			
